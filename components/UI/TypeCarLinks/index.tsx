732b5582fa80b6f976fae68314d5198e563d016d
roma <roma-lysyk@ukr.net>
2025-05-19 15:54:25 +0300

    fix

--- components/UI/TypeCarLinks/index.tsx
+++ components/UI/TypeCarLinks/index.tsx
@@ -49,7 +49,7 @@ const LinkComponent: FC<ILinkComponent> = (
 	const dispatch = useAppDispatch();
 
 	const handleClick = () => {
-		onClick && onClick();
+		if(onClick) onClick();
 		dispatch(resetFilter());
 		dispatch(setParams({ 'vehicle_type': null }));
 	}
